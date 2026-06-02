5eaff01eb4c3fffd3c47cf8f0d66f6c8e45ce944
Lenovo <Lenovo@DESKTOP-LEHG4DH>
2026-06-02 14:44:46 +0530

    latest

--- SauceDemo/src/test/java/sauceDemoBase/Base.java
+++ SauceDemo/src/test/java/sauceDemoBase/Base.java
@@ -1,17 +1,22 @@
 package sauceDemoBase;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
 import constants.Constants;
+import utilities.ScreenshotUtility;
 
 public class Base {
 
@@ -26,7 +31,8 @@ public class Base {
 		f=new FileInputStream(Constants.CONFIGFILE);
 		prop.load(f);
 		if (browsers.equalsIgnoreCase("Chrome")) {
-			driver = new ChromeDriver();
+			ChromeOptions options=disablePasswordManager();
+			driver = new ChromeDriver(options);
 		} else if (browsers.equalsIgnoreCase("firefox")) {
 			driver = new FirefoxDriver();
 		} else if (browsers.equalsIgnoreCase("edge")) {
@@ -39,8 +45,24 @@ public class Base {
 
 	}
 	
+	public ChromeOptions disablePasswordManager() {
+		Map<String, Object> chromePrefs = new HashMap<>();
+		chromePrefs.put("credentials_enable_service", false);
+		chromePrefs.put("profile.password_manager_enabled", false);
+		chromePrefs.put("profile.password_manager_leak_detection", false);
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("prefs", chromePrefs);
+		return chromeOptions;
+	}
+	
 	@AfterMethod(alwaysRun = true)
-	public void driverClose() {
+	public void driverClose(ITestResult iTestResult) throws IOException {
+		if (iTestResult.getStatus() == ITestResult.FAILURE) {
+
+			ScreenshotUtility screenShot = new ScreenshotUtility();
+			screenShot.getScreenshot(driver, iTestResult.getName());
+		}
 		driver.quit();
 	}
 }
